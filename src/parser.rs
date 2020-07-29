34a73e17c7e72988ea38092664bac751e1ce2ec9
Tony Solomonik <tony.solomonik@gmail.com>
2020-07-29 23:35:20 +0300

    parser: export expression logic to functions

--- src/parser.rs
+++ src/parser.rs
@@ -37,13 +37,74 @@ impl Parser {
         Some(token)
     }
 
+    fn get_special_char_expression(&mut self, prev: Box<Expression>, token: Token) -> Option<Box<Expression>> {
+        let c = token.value.as_bytes()[0] as char;
+        match c {
+            ';' => Some(prev),
+            '=' => {
+                if let Some(prev_token) = prev.get_token() {
+                    if prev_token.typ.type_id() != TokenType::Symbol.type_id() {
+                        println!("Error: assignment: {} is not a valid symbol", prev_token.value);
+                        None
+                    } else {
+                        if let Some(next) = self.next_expression(None) {
+                            Some(Box::new(Expression::Operation(
+                                prev,
+                                Token { typ: TokenType::Assignment, value: token.value },
+                                next,
+                            )))
+                        } else {
+                            None
+                        }
+                    }
+                } else {
+                    println!("Error: assignment: could not get a token from prev");
+                    None
+                }
+            }
+            _ => {
+                println!("Error: special: found an unknown character");
+                None
+            }
+        }
+    }
+
+    fn get_operation_expression(&mut self, prev: Box<Expression>, token: Token) -> Option<Box<Expression>> {
+        match self.next_expression(None) {
+            Some(subtree) => {
+                let priority = Parser::get_operation_priority(&token);
+
+                if let Expression::Operation(left, subtree_token, right) = *subtree {
+                    let subtree_priority = Parser::get_operation_priority(&subtree_token);
+                    if priority > 0 && subtree_priority > 0 && priority > subtree_priority {
+                        // Create a rotated left operation tree
+                        Some(Box::new(Expression::Operation(
+                            Box::new(Expression::Operation(prev, token, left)),
+                            subtree_token,
+                            right)))
+                    } else {
+                        Some(Box::new(Expression::Operation(
+                            prev,
+                            token,
+                            Box::new(Expression::Operation(left, subtree_token, right))))
+                        )
+                    }
+                } else {
+                    Some(Box::new(Expression::Operation(prev, token, subtree)))
+                }
+            }
+            None => {
+                println!("Error: operation: could not find an expression after {}", token.value);
+                None
+            }
+        }
+    }
+
     fn next_expression(&mut self, option_prev: Option<Box<Expression>>) -> Option<Box<Expression>> {
         let token = match self.eat_token() {
             Some(x) => x,
             None => return None,
         };
-
-        let token_value = token.value.clone();
 
         let type_id = token.typ.type_id();
         if option_prev.is_none() && (type_id == TokenType::Number.type_id() || type_id == TokenType::Symbol.type_id()) {
@@ -51,73 +112,13 @@ impl Parser {
             return self.next_expression(Some(Box::new(simple_node)));
         };
 
+        let token_value = token.value.clone();
+
         let result_tree = match option_prev {
             Some(prev) => {
                 match token.typ {
-                    TokenType::Special => {
-                        let c = token.value.as_bytes()[0] as char;
-                        match c {
-                            ';' => Some(prev),
-                            '=' => {
-                                if let Some(prev_token) = prev.get_token() {
-                                    if prev_token.typ.type_id() != TokenType::Symbol.type_id() {
-                                        println!("Error: assignment: {} is not a valid symbol", prev_token.value);
-                                        None
-                                    } else {
-                                        if let Some(next) = self.next_expression(None) {
-                                            Some(Box::new(Expression::Operation(
-                                                prev,
-                                                Token { typ: TokenType::Assignment, value: token.value.clone() },
-                                                next,
-                                            )))
-                                        } else {
-                                            None
-                                        }
-                                    }
-                                } else {
-                                    println!("Error: assignment: could not get a token from prev");
-                                    None
-                                }
-                            }
-                            _ => {
-                                println!("Error: special: found an unknown character");
-                                None
-                            }
-                        }
-                    }
-
-                    TokenType::Operation => {
-                        match self.next_expression(None) {
-                            Some(subtree) => {
-                                let priority = Parser::get_operation_priority(&token);
-
-                                if let Expression::Operation(left, subtree_token, right) = *subtree {
-                                    let subtree_priority = Parser::get_operation_priority(&subtree_token);
-                                    if priority > 0 && subtree_priority > 0 && priority > subtree_priority {
-                                        // let operation_tree = Tree { token, left: prev, right: x };
-
-                                        // Create a rotated left operation tree
-                                        Some(Box::new(Expression::Operation(
-                                            Box::new(Expression::Operation(prev, token, left)),
-                                            subtree_token,
-                                            right)))
-                                    } else {
-                                        Some(Box::new(Expression::Operation(
-                                            prev,
-                                            token,
-                                            Box::new(Expression::Operation(left, subtree_token, right))))
-                                        )
-                                    }
-                                } else {
-                                    Some(Box::new(Expression::Operation(prev, token, subtree)))
-                                }
-                            }
-                            None => {
-                                println!("Error: operation: could not find an expression after {}", token_value);
-                                None
-                            }
-                        }
-                    }
+                    TokenType::Special => self.get_special_char_expression(prev, token),
+                    TokenType::Operation => self.get_operation_expression(prev, token),
                     _ => {
                         println!("Error: found an unknown token");
                         None
